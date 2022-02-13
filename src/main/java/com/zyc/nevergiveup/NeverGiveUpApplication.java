30f24c4900ce5130cdaf5db5192d418ac235e6a6
zhangyanchang <zhangyanchang@persagy.com>
2022-02-13 14:00:56 +0800

    首次提交

---
+++ src/main/java/com/zyc/nevergiveup/NeverGiveUpApplication.java
@@ -0,0 +1,13 @@
+package com.zyc.nevergiveup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeverGiveUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeverGiveUpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zyc/nevergiveup/NeverGiveUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyc.nevergiveup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeverGiveUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
